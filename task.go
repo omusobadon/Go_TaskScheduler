8311864bc262432c07df73338714b4f522acb76f
22cc0207 <22cc0207@jec.ac.jp>
2023-12-11 00:56:36 +0900

    12-10-02

--- task.go
+++ task.go
@@ -10,13 +10,22 @@ import (
 func task(c *db.PrismaClient, s db.StockModel) error {
 	ctx := context.Background()
 
-	// Delete
-	_, err := c.Stock.FindUnique(
-		db.Stock.ID.Equals(s.ID),
-	).Delete().Exec(ctx)
+	// 在庫数をデフォルトの個数にリセット
+	// test, err := c.Stock.FindUnique(
+	// 	db.Stock.ID.Equals(s.ID),
+	// ).Exec(ctx)
+	// if err != nil {
+	// 	return fmt.Errorf("在庫テーブル削除エラー : %w", err)
+	// }
+
+	test, err := c.Product.FindMany(
+		db.Product.ID.Equals(1),
+	).Exec(ctx)
 	if err != nil {
-		return fmt.Errorf("在庫テーブル削除エラー : %w", err)
+		return fmt.Errorf("テストエラー : %w", err)
 	}
+
+	fmt.Printf("test : %+v", test[0].Stock())
 
 	return nil
 }
